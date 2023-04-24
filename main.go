f5f5924071076590e5666b6ee3da848bd4f5bd17
yinshuaibo <1359433661@qq.com>
2023-04-24 15:19:21 +0800

    add feishu content

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("hello world!!!!1!")
+
+	resp, err := http.Post("https://open.feishu.cn/open-apis/bot/v2/hook/f662967e-7ec7-4cf2-b438-13b94bd4f85c",
+		"application/json",
+		strings.NewReader("{\"msg_type\":\"text\",\"content\":{\"text\":\"构建成功\"}}"))
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp.Status)
 }
