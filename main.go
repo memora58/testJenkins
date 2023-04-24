446e2438c46fe1e73d3dc7aa8d8906c276cbf3ac
yinshuaibo <1359433661@qq.com>
2023-04-24 16:47:45 +0800

    feishu content

--- main.go
+++ main.go
@@ -10,9 +10,10 @@ func main() {
 
 	resp, err := http.Post("https://open.feishu.cn/open-apis/bot/v2/hook/8732a64a-a2b0-4632-b1f5-8a70dea49251",
 		"application/json",
-		strings.NewReader("{\"msg_type\":\"text\",\"content\":{\"text\":\"构建成功1\"}}"))
+		strings.NewReader("{\"msg_type\":\"text\",\"content\":{\"text\":\"构建成功\"}}"))
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Println(resp.Status)
+
 }
